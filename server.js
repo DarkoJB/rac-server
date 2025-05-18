ab8a83df09d9a9b19fc1adfacda85dfdeeecfd6f
Darko Božić <darkojbozic@gmail.com>
2025-05-18 18:01:35 +0200

    Remove unused dependencies

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const serverless = require("serverless-http");
 
 require("dotenv").config();
 
